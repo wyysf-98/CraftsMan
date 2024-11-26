da5cc9c1d990944d0e7e24c40a2760211993893c
liyuuuuuuu_ <31622768+wyysf-98@users.noreply.github.com>
2024-11-26 22:42:30 +0800

    Update inference.py

--- inference.py
+++ inference.py
@@ -2,10 +2,10 @@ from craftsman import CraftsManPipeline
 import torch
 
 # load from local ckpt
-pipeline = CraftsManPipeline.from_pretrained("./ckpts/craftsman-v1-5", device="cuda:0", torch_dtype=torch.float32) 
+pipeline = CraftsManPipeline.from_pretrained("./ckpts/craftsman", device="cuda:0", torch_dtype=torch.float32) 
 
 # # load from huggingface model hub
-# pipeline = CraftsManPipeline.from_pretrained("craftsman3d/craftsman-v1-5", device="cuda:0", torch_dtype=torch.float32)
+# pipeline = CraftsManPipeline.from_pretrained("craftsman3d/craftsman", device="cuda:0", torch_dtype=torch.float32)
 
 image_file = "val_data/werewolf.png"
 obj_file = "werewolf.glb" # output obj or glb file
